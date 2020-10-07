69cf8c3d675d493fb6e775dc33008067d0bc2bb3
Dendrocopos-kr <lunatic787973@gmail.com>
2020-10-07 10:52:27 +0900

    calc 오류 수정

---
+++ work_one/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,24 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+
+import kr.itedu.ex10.Calc;
+
+class CalcTest {
+	private int n1, n2, sum;
+
+	@BeforeEach
+	void init() {
+		n1 = (int)(Math.random()*10);
+		n2 = (int)(Math.random()*10);
+		sum = n1 + n2;
+	}
+	
+	@RepeatedTest(value = 1000)
+	void test() {
+		assertEquals(Calc.sum(n1, n2),sum);
+	}
+}
